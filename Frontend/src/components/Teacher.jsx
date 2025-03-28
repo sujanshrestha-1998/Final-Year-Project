00d42dfa926fafeb688bdef84acd8c599e55d797
sujanshrestha-1998 <sujanstha.0122@gmail.com>
2025-03-28 19:32:06 +0545

    Updated: Updated Teacher Section

--- Frontend/src/components/Teacher.jsx
+++ Frontend/src/components/Teacher.jsx
@@ -1,118 +1,121 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
+import { IoSearch } from "react-icons/io5";
 import { IoMdInformationCircleOutline } from "react-icons/io";
-import { FaRegAddressCard } from "react-icons/fa";
-import { useNavigate } from "react-router-dom";
-
-const TeacherList = ({ teachers, selectedTeacher, onTeacherClick }) => {
-  return (
-    <div className="max-h-[500px] overflow-y-auto rounded-[8px] shadow-sm">
-      {teachers.map((teacher) => (
-        <div
-          key={teacher.teacher_id}
-          className={`border-b border-gray-300 p-2 shadow-md cursor-pointer ${
-            selectedTeacher && selectedTeacher.teacher_id === teacher.teacher_id
-              ? "bg-blue-500 text-white"
-              : "bg-white text-black"
-          }`}
-          onClick={() => onTeacherClick(teacher)}
-        >
-          <p className="text-md font-semibold">
-            {teacher.first_name} {teacher.last_name}
-          </p>
-          <p className="text-sm">{teacher.email}</p>
-        </div>
-      ))}
-    </div>
-  );
-};
 
 const Teacher = () => {
-  const [loading, setLoading] = useState(false);
-  const [teachers, setTeachers] = useState([]);
-  const [error, setError] = useState("");
-  const [selectedTeacher, setSelectedTeacher] = useState(null);
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const fetchTeachers = async () => {
-      try {
-        const response = await axios.get(
-          "http://localhost:3000/api/teacher_details"
-        );
-        setTeachers(response.data.teachers);
-        setError("");
-
-        if (response.data.teachers.length > 0) {
-          setSelectedTeacher(response.data.teachers[0]);
-        }
-      } catch (err) {
-        console.error("Error fetching teacher details:", err);
-        setError(
-          "Error fetching teacher details. " +
-            (err.response ? err.response.data : err.message)
-        );
-        setTeachers([]);
-      }
-    };
-
-    fetchTeachers();
-  }, []);
-
-  const handleRegisterClick = () => {
-    setLoading(true);
-    setTimeout(() => {
-      setLoading(false);
-      navigate("/registerteacher");
-    }, 1500);
-  };
-
   return (
-    <div>
-      <div className="h-screen bg-[#f2f1f1]">
-        <div className="mx-8">
-          <div className="flex items-center gap-2">
-            <h1 className="text-2xl font-bold">TEACHER DETAILS</h1>
+    <div className="h-screen w-full overflow-hidden">
+      <div className="mx-8 w-full overflow-auto">
+        {/* Top Section */}
+        <div className="flex items-center gap-4 ">
+          <div className="flex items-center gap-2 py-5">
+            <h1 className="font-semibold text-2xl text-black">CAMPUS MAP</h1>
             <IoMdInformationCircleOutline className="text-2xl" />
           </div>
-          <div className="flex mt-5">
-            <button
-              onClick={handleRegisterClick}
-              className="bg-blue-500 rounded-[8px] px-6 py-1 flex justify-center font-medium items-center gap-2 text-white transform transition-all duration-300"
-            >
-              {loading ? (
-                <div className="loader w-6 h-6 bg-blue-500 border-white rounded-full animate-spin"></div>
-              ) : (
-                <>
-                  Register Teacher
-                  <FaRegAddressCard className="bg-blue-500 text-lg" />
-                </>
-              )}
-            </button>
+          <div className="relative w-80">
+            <IoSearch className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500" />
+            <input
+              type="text"
+              placeholder="Search"
+              className="w-full pl-8 pr-4 py-1 bg-gray-200 rounded-md 
+               text-[14px] border-none 
+               transition-all duration-200 placeholder-gray-500"
+            />
           </div>
-          {error && <p className="text-red-600 font-bold">{error}</p>}
-          <div className="flex w-full gap-20">
-            <div className="w-1/3">
-              {teachers.length > 0 ? (
-                <div className="mt-6">
-                  <h2 className="text-xl font-bold mb-4">All Teachers</h2>
-                  <TeacherList
-                    teachers={teachers}
-                    selectedTeacher={selectedTeacher}
-                    onTeacherClick={(teacher) => setSelectedTeacher(teacher)}
-                  />
+        </div>
+
+        {/* Bottom Section - Using Flexbox to position sections side by side */}
+        <div className="flex gap-8">
+          {/* Left Section */}
+          <div>
+            {/* Second Floor */}
+            <div className="z-30 relative">
+              <div className="flex flex-col gap-5">
+                <h1>Administration Block</h1>
+                <div className="w-[620px] h-0.5 bg-black"></div>
+              </div>
+              <div className="flex flex-col gap-8 ">
+                <div className="mt-4 flex gap-8">
+                  <h1 className="text-gray-400">
+                    Second <br /> Floor
+                  </h1>
+                  <div className="flex gap-5">
+                    <div className="w-40 h-48 bg-[#92bd63] shadow-lg shadow-gray-800/50">
+                      <h1 className="text-white ml-2 font-medium mt-2">
+                        Academics E
+                      </h1>
+                    </div>
+                    <div className="w-40 h-48 bg-[#92bd63] shadow-lg shadow-gray-800/50"></div>
+                    <div className="w-40 h-48 border border-gray-400 shadow-lg bg-white shadow-gray-800/50"></div>
+                  </div>
                 </div>
-              ) : (
-                <p className="text-gray-600 mt-6">No teachers found</p>
-              )}
+                <div className="w-[620px] h-0.5 bg-black  "></div>
+              </div>
             </div>
-            <div className="w-full">
-              <img
-                src="/src/assets/TeacherDashboard.png"
-                alt=""
-                width={900}
-                height={900}
-              />
+
+            {/* First Floor */}
+            <div className="z-20 relative mt-[-80px]">
+              <div className="flex flex-col gap-8 ">
+                <div className="mt-4 flex gap-8">
+                  <h1 className="text-gray-400 mt-20">
+                    First <br /> Floor
+                  </h1>
+                  <div className="flex gap-5">
+                    <div className="w-64 h-36 bg-[#92bd63] shadow-lg shadow-gray-800/50">
+                      <h1 className="text-white ml-2 font-medium mt-28">
+                        Academics E
+                      </h1>
+                    </div>
+                    <div className="w-56 h-36 border bg-white border-gray-400 shadow-lg shadow-gray-800/50"></div>
+                  </div>
+                </div>
+                <div className="w-[620px] h-0.5 bg-black"></div>
+              </div>
+            </div>
+
+            {/* Ground Floor */}
+            <div className="z-10 relative mt-[-80px]">
+              <div className="flex flex-col gap-8">
+                <div className="mt-4 flex gap-8">
+                  <h1 className="text-gray-400 mt-20">
+                    Ground <br /> Floor
+                  </h1>
+                  <div className="flex gap-5">
+                    <div className="w-36 h-36 bg-white border-gray-400 shadow-lg shadow-gray-800/50">
+                      <h1 className=" ml-2 font-medium mt-24">
+                        RTE Department
+                      </h1>
+                    </div>
+                    <div className="w-36 h-60 bg-white border-gray-400 shadow-lg shadow-gray-800/50">
+                      <h1 className=" ml-2 font-medium mt-44">
+                        Student Department
+                      </h1>
+                    </div>
+                    <div className="w-44 h-36 bg-[#92bd63]  shadow-lg shadow-gray-800/50"></div>
+                  </div>
+                </div>
+                <div className="w-[620px] h-0.5 bg-black"></div>
+              </div>
+            </div>
+            <div className="mt-20 ml-10">
+              <div className="w-[640px] h-48 bg-white border border-gray-400 p-4 shadow-lg shadow-gray-800/50">
+                <h1>Wolverhampton Block</h1>
+              </div>
+            </div>
+          </div>
+
+          {/* Middle Section */}
+          <div>
+            <div className="bg-gray-200 h-[800px] w-[150px] shadow-lg shadow-gray-800/50">
+              <h1 className="font-medium p-4">Canteen</h1>
+            </div>
+          </div>
+
+          {/* Right Section */}
+          <div>
+            <div className="mt-56">
+              <div className="z-20 relative w-52 h-72 border border-gray-400 shadow-lg shadow-gray-800/50"></div>
+              <div className="z-10 relative mt-[-260px] ml-5 w-36 h-48 bg-[#92bd63] shadow-lg shadow-gray-800/50"></div>
             </div>
           </div>
         </div>
